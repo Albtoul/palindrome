286cb826a99f443f5182680d161d500d50f1a269
albtoul <albtoul-kaseem@hotmail.com>
2021-10-09 18:13:29 +0300

    palindrome

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+    public static boolean Palindrome(String a){
+        if(a.length() == 0 || a.length() == 1){
+            return true;
+        }
+        if(a.charAt(0) == a.charAt(a.length()-1)){
+            return Palindrome(a.substring(1, a.length()-1));
+        }
+        return false;
+    }
+
+    public static void main(String[]args){
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Enter the string for check : ");
+        String string = inp.nextLine();
+        if(Palindrome(string)){
+            System.out.println(string + " is a palindrome");
+        }
+        else{
+            System.out.println(string + " is not a palindrome");
+        }
+    }
+}
